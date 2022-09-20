6f561f5c2287b22466e2709ef8894038f718e0d1
Steven Xue <stevenxue2001@gmail.com>
2022-09-19 20:38:11 -0400

    asdf

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -132,7 +132,7 @@ while True:
 
         welcome = "Welcome to CT Calender"
         y = top
-        draw.text((x, y), welcome, font=font, fill=0)
+        draw.text((x+width, y+width), welcome, font=font, fill=0)
 
     if not buttonA.value and buttonB.value:
         welcome = "Hello, today is " + weekDayName[weekday]
